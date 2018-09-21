692151b03620fc92de65438facdfaad3a8fe5f42
Administrator <Administrator@WIN-6SBV0QG4LEF>
2018-09-21 08:32:12 +0800

    2018/9/21

---
+++ admin-dashboard/app/store/NavigationTree.js
@@ -0,0 +1,28 @@
+﻿Ext.define('Admin.store.NavigationTree', {
+    extend: 'Ext.data.TreeStore',
+
+    storeId: 'NavigationTree',
+
+    fields: [{
+        name: 'text'
+    }],
+
+    root: {
+        expanded: true,
+        children: [
+            {
+                text: 'Dashboard',
+                iconCls: 'x-fa fa-desktop',
+                viewType: 'admindashboard',
+                routeId: 'dashboard', // routeId defaults to viewType
+                leaf: true
+            },{
+                text: '模板',
+                iconCls: 'x-fa fa-address-card',
+                //rowCls: 'nav-tree-badge nav-tree-badge-new',
+                viewType: 'user',
+                leaf: true
+            }
+        ]
+    }
+});
