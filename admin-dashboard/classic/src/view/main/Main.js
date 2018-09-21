430769d8b0297b09c3bf8d57996b1e84a9f2d8b0
Administrator <Administrator@WIN-6SBV0QG4LEF>
2018-09-21 08:35:16 +0800

    2018/9/20

---
+++ admin-dashboard/classic/src/view/main/Main.js
@@ -0,0 +1,109 @@
+﻿Ext.define('Admin.view.main.Main', {
+    extend: 'Ext.container.Viewport',
+
+    requires: [
+        'Ext.button.Segmented',
+        'Ext.list.Tree'
+    ],
+
+    controller: 'main',
+    viewModel: 'main',
+
+    cls: 'sencha-dash-viewport',
+    itemId: 'mainView',
+
+    layout: {
+        type: 'vbox',
+        align: 'stretch'
+    },
+
+    listeners: {
+        render: 'onMainViewRender'
+    },
+
+    items: [
+        {
+            xtype: 'toolbar',
+            cls: 'sencha-dash-dash-headerbar shadow',
+            height: 64,
+            itemId: 'headerBar',
+            items: [
+                {
+                    xtype: 'component',
+                    reference: 'senchaLogo',
+                    cls: 'sencha-logo',
+                    html: '<div class="main-logo"><img src="resources/images/company-logo.png">Knorr Agency</div>',
+                    width: 250
+                },
+                {
+                    margin: '0 0 0 8',
+                    ui: 'header',
+                    iconCls:'x-fa fa-navicon',
+                    id: 'main-navigation-btn',
+                    handler: 'onToggleNavigationSize'
+                },
+                '->',
+                {
+                    /*x-fa fa-envelope fa-5x*/
+                    iconCls:'fa fa-envelope fa-5x',
+                    ui: 'header',
+                    href: '#email',
+                    hrefTarget: '_self',
+                    tooltip: '查看 email'
+                },
+                {
+                    iconCls:'fa fa-th-large fa-5x',
+                    ui: 'header',
+                    href: '#profile',
+                    hrefTarget: '_self',
+                    tooltip: '个人空间'
+                },
+                /*{
+                    xtype: 'tbtext',
+                    text: '用户名:Admin',
+                    cls: 'top-user-name'
+                },*/
+                {
+                    xtype: 'image',
+                    cls: 'header-right-profile-image',
+                    height: 35,
+                    width: 35,
+                    alt:'current user image',
+                    src: 'resources/images/user-profile/2.png'
+                }
+            ]
+        },
+        {
+            xtype: 'maincontainerwrap',
+            id: 'main-view-detail-wrap',
+            reference: 'mainContainerWrap',
+            flex: 1,
+            items: [
+                {
+                    xtype: 'treelist',
+                    reference: 'navigationTreeList',
+                    itemId: 'navigationTreeList',
+                    ui: 'nav',
+                    store: 'NavigationTree',
+                    width: 250,
+                    expanderFirst: false,
+                    expanderOnly: false,
+                    listeners: {
+                        selectionchange: 'onNavigationTreeSelectionChange'
+                    }
+                },
+                {
+                    xtype: 'container',
+                    flex: 1,
+                    reference: 'mainCardPanel',
+                    cls: 'sencha-dash-right-main-container',
+                    itemId: 'contentPanel',
+                    layout: {
+                        type: 'card',
+                        anchor: '100%'
+                    }
+                }
+            ]
+        }
+    ]
+});
