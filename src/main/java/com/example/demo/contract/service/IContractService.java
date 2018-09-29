5a0e9e48e40959d3c74dbe4fd0093cd92aa56581
kinghou <kinghou@DESKTOP-CB7553S>
2018-09-30 07:46:50 +0800

    添加合同审核

--- src/main/java/com/example/demo/contract/service/IContractService.java
+++ src/main/java/com/example/demo/contract/service/IContractService.java
@@ -1,5 +1,6 @@
 package com.example.demo.contract.service;
 
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.data.domain.Page;
@@ -7,20 +8,36 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 
 import com.example.demo.contract.entity.Contract;
+import com.example.demo.contract.entity.ContractDTO;
 
 public interface IContractService {
 	
+	/*合同业务*/
 	public void save(Contract contract);
 	
 	public void deleteById(Long id);
 	
-	public Optional<Contract> findById(Long id);
-	
 	public void deleteAll(Long[] ids);
+	
+	public Optional<Contract> findById(Long id);
 	
 	public Page<Contract> findAll(Specification<Contract> spec, Pageable pageable);
 	
 	/*上传word文档*/
 	public Contract readWord(String buffer);
+	
+	
+	/*合同审核业务*/
+	//1.启动流程
+	public void startWorkflow(String userId,Long leaveId, Map<String, Object> variables);
+	
+	//2.查询流程任务
+	public Page<ContractDTO> findTodoTasks(String userId, Pageable pageable);
+	
+	//3.签收流程任务
+	public void claim(String taskId,String userId);
+	
+	//4.完成流程任务
+	public void complete(String taskId, Map<String, Object> variables); 
 
 }
