dc5e022246524b2d0b4cadbd678a4ef5801ecacd
jie <jie@Lenovo-PC>
2018-10-13 21:17:37 +0800

    change

--- src/test/java/com/example/demo/AddreeListServiceTest.java
+++ src/test/java/com/example/demo/AddreeListServiceTest.java
@@ -6,27 +6,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.example.demo.addressList.entity.Employee;
-import com.example.demo.addressList.repository.AddressListRepository;
-import com.example.demo.addressList.service.IAddressListService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AddreeListServiceTest {
-	@Autowired
-    private	IAddressListService addressListService;
-	@Test
-	public void insertDate() {
-		for(int i=0;i<100;i++) {
-			Employee tmp=new Employee();
-			tmp.setEmployeeName("小明"+i);
-			tmp.setEmployeeNumber(""+i);
-			tmp.setEmployeeArea("第"+i+"地区");
-			tmp.setPost("路人"+i);
-			tmp.setEmail("1234567"+i+"@qq.com");
-			addressListService.save(tmp);
-		}
-
-	}
+//	@Autowired
+//    private	IAddressListService addressListService;
+//	@Test
+//	public void insertDate() {
+//		for(int i=0;i<100;i++) {
+//			Employee tmp=new Employee();
+//			tmp.setEmployeeName("小明"+i);
+//			tmp.setEmployeeNumber(""+i);
+//			tmp.setEmployeeArea("第"+i+"地区");
+//			tmp.setPost("路人"+i);
+//			tmp.setEmail("1234567"+i+"@qq.com");
+//			addressListService.save(tmp);
+//		}
+//
+//	}
 
 }
