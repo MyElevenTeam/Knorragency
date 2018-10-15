930ae355e9eb3bc75dde7ba96f04a8c71b3b2828
Administrator <Administrator@WIN-6SBV0QG4LEF>
2018-10-15 08:29:29 +0800

    修改

--- src/main/java/com/example/demo/employee/config/Config.java
+++ src/main/java/com/example/demo/employee/config/Config.java
@@ -19,20 +19,5 @@ public class Config extends WebMvcConfigurerAdapter{
 //		registry.addInterceptor(employeeConfig).addPathPatterns("/store/**");
 //		registry.addInterceptor(employeeConfig).addPathPatterns("/employee/**");
 //	}
-	
-	
-	
-	/*@Autowired
-	private EmployeeConfig employeeConfig;
-	@Autowired
-	private EmployeeConfig2 employeeConfig2;
-	
-	@Override
-	public void addInterceptors(InterceptorRegistry registry) {
-		//registry.addInterceptor(employeeConfig2).addPathPatterns("/**");
-		registry.addInterceptor(employeeConfig).addPathPatterns("/store/**");
-		registry.addInterceptor(employeeConfig).addPathPatterns("/employee/**");
-	}*/
 
-	
 }
--- src/main/java/com/example/demo/system/controller/LoginController.java
+++ src/main/java/com/example/demo/system/controller/LoginController.java
@@ -73,9 +73,6 @@ public class LoginController {
     }
     
     
-//<<<<<<< HEAD
-    
-
 //	private static final Logger log = LoggerFactory.getLogger(LoginController.class);
 //	
 //	@Autowired
@@ -131,69 +128,5 @@ public class LoginController {
 //		}
 //		
 //	}
-    
-    
-    
-    
-    
-    
-    
-    
-
-   // private static final Logger log = LoggerFactory.getLogger(LoginController.class);
-//	
-//	@Autowired
-//	private EmployeeRepository employeeRepository;
-//	
-//	//登录
-//	@RequestMapping(value="/login")
-//	public @ResponseBody ExtAjaxResponse logon(@RequestParam("employeeNumber") String employeeNumber
-//			, @RequestParam("password") String password, HttpSession session) {
-//		Employee employee=employeeRepository.findByEmployeeNumber(employeeNumber);
-//		if(employee==null || !employee.getPassword().equals(password)) {
-//			return new ExtAjaxResponse(false,"登录失败!帐号或者密码有误!请重新登录!");
-//		}else {
-//			session.setAttribute("employeeNumber", employee.getEmployeeNumber());
-//			session.setAttribute("post", employee.getPost());
-//			session.setAttribute("picture", employee.getPicture());
-//			/*Map<String,String> map=new HashMap<String,String>();
-//			map.put("employeeNumber", employee.getEmployeeNumber());
-//			map.put("picture", employee.getPicture());
-//			map.put("msg", "登录成功！");*/
-//			
-//			return new ExtAjaxResponse(true,"登录成功");
-//		}
-//	}
-//	
-//	//获取登录后的信息
-//	@RequestMapping(value="/getLoginMessage")
-//	public @ResponseBody ExtAjaxResponse getLoginMessage(HttpServletRequest request) {
-//		/*log.info("进来了/login/getLoginMessage");
-//		String en=(String) request.getSession().getAttribute("employeeNumber");
-//		String picture=(String) request.getSession().getAttribute("picture");
-//		log.info(en);
-//		log.info(picture);*/
-//		log.info("进来了/login/getLoginMessage");
-//		Map<String,String> map=new HashMap<String,String>();
-//		map.put("employeeNumber", (String) request.getSession().getAttribute("employeeNumber"));
-//		map.put("picture", (String) request.getSession().getAttribute("picture"));
-//		
-//		return new ExtAjaxResponse(true,map);
-//	}
-//	
-//	//注销
-//	@RequestMapping(value = "/logout")
-//	public @ResponseBody ExtAjaxResponse logout(HttpSession session) {
-//		
-//		try {
-//			log.info("-----------------------------------------logout");
-//			session.removeAttribute("employeeNumber");
-//			session.removeAttribute("post");
-//			return new ExtAjaxResponse(true,"登出成功!");
-//		} catch (Exception e) {
-//			return new ExtAjaxResponse(true,"登出失败!");
-//		}
-//		
-//	}
-//>>>>>>> branch 'master' of https://github.com/MyElevenTeam/Knorragency.git
+ 
 }
