aaaa35d7287f6c74e11344092b8604658d23c9e5
215146962 <215146962@qq.com>
2018-10-15 17:20:51 +0800

    employee的增删改查和登录添加了关于工作流的一些操作，可以运行，但具体功能没测试，可能报错

--- src/main/java/com/example/demo/system/controller/LoginController.java
+++ src/main/java/com/example/demo/system/controller/LoginController.java
@@ -19,57 +19,66 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.common.controller.ExtAjaxResponse;
 import com.example.demo.common.controller.SessionUtil;
+import com.example.demo.employee.domain.Employee;
+import com.example.demo.employee.repository.EmployeeRepository;
 
 
 @RestController
 public class LoginController {
- 	private static Logger logger = LoggerFactory.getLogger(LoginController.class);
-    @Autowired
+	
+	private static final Logger log = LoggerFactory.getLogger(LoginController.class);
+	
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	@Autowired
     private IdentityService identityService;
-    /**
-     * 登录系统
-     **/
-    @RequestMapping(value = "/login")
-    public @ResponseBody ExtAjaxResponse logon(@RequestParam("userName") String userName, @RequestParam("password") String password, HttpSession session) {
-    	logger.debug("logon request: {userName={}, password={}}", userName, password);
-        boolean checkPassword = identityService.checkPassword(userName, password);
-        if (checkPassword) {
-            // 查看用户是否存在
-            User user = identityService.createUserQuery().userId(userName).singleResult();
+	//登录
+	@RequestMapping(value="/login")
+	public @ResponseBody ExtAjaxResponse logon(@RequestParam("userName") String employeeNumber
+			, @RequestParam("password") String password, HttpSession session) {
+		Employee employee=employeeRepository.findByEmployeeNumber(employeeNumber);
+		if(employee==null || !employee.getPassword().equals(password)) {
+			return new ExtAjaxResponse(false,"登录失败!帐号或者密码有误!请重新登录!");
+		}else {
+			//老师自带的activiti
+			User user = identityService.createUserQuery().userId(employeeNumber).singleResult();
             SessionUtil.setUser(session, user);
 	  
 	        //读取角色Group
             List<Group> groupList = identityService.createGroupQuery().groupMember(user.getId()).list();
 
             SessionUtil.setGroupList(session, groupList);
-
+            
+            SessionUtil.setEmployeeNumber(session, employee.getEmployeeNumber());
+            SessionUtil.setPost(session, employee.getPost());
+            
             String[] groupNames = new String[groupList.size()];
             for (int i = 0; i < groupNames.length; i++) {
                 groupNames[i] = groupList.get(i).getName();
             }
             SessionUtil.setGroupNames(session, ArrayUtils.toString(groupNames));//"groupNames"  : "admin,hrManager"
-            
-            Map<String,String> map=new HashMap<String, String>();
-            map.put("userName", userName);
-            map.put("msg", "登录成功!");
-            //map.put("loginUserImage", "imgUrl");
-            return new ExtAjaxResponse(true,map);
-        } else {
-        	return new ExtAjaxResponse(false,"登录失败!帐号或者密码有误!请重新登录!");
-        }
-    }
-    /**
-     * 退出登录
-     */
-    @RequestMapping(value = "/logout")
-    public @ResponseBody ExtAjaxResponse logout(HttpSession session) 
-    {
-    	try {
-    		SessionUtil.removeAttribute(session);
-        	return new ExtAjaxResponse(true,"登出成功!");
-		} catch (Exception e) {
-			return new ExtAjaxResponse(false,"登出失败!");
+            //------------------end-----------
+			
+			
+			Map<String,String> map=new HashMap<String,String>();
+			map.put("employeeNumber", employee.getEmployeeNumber());
+			map.put("picture", employee.getPicture());
+			map.put("msg", "登录成功！");
+			
+			return new ExtAjaxResponse(true,"登录成功");
 		}
-    }
- 
+	}
+	
+	//注销
+	@RequestMapping(value = "/logout")
+	public @ResponseBody ExtAjaxResponse logout(HttpSession session) {
+		
+		try {
+			SessionUtil.removeAttribute(session);
+			return new ExtAjaxResponse(true,"登出成功!");
+		} catch (Exception e) {
+			return new ExtAjaxResponse(true,"登出失败!");
+		}
+		
+	}
 }
