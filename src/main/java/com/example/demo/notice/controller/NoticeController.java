2c4b4f64d8a7d9f1ce1ceeb27065f8f244ac3414
Administrator <Administrator@WIN-6SBV0QG4LEF>
2018-10-17 21:08:48 +0800

    公告栏

---
+++ src/main/java/com/example/demo/notice/controller/NoticeController.java
@@ -0,0 +1,33 @@
+package com.example.demo.notice.controller;
+
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.attence.entity.AttenceDTO;
+import com.example.demo.attence.entity.AttenceQueryDTO;
+import com.example.demo.common.controller.ExtjsPageRequest;
+import com.example.demo.common.controller.SessionUtil;
+import com.example.demo.log.config.SystemControllerLog;
+import com.example.demo.notice.entity.Notice;
+import com.example.demo.notice.service.INoticeService;
+
+@RestController
+@RequestMapping("/notice")
+public class NoticeController {
+	@Autowired
+	INoticeService noticeService;
+	@SystemControllerLog(description="查看公告")
+	@GetMapping
+	public Page<Notice> findNotice(ExtjsPageRequest extjsPageRequest) {
+	   return noticeService.findAll(null, extjsPageRequest.getPageable());
+	}
+
+}
